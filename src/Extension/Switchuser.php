4f6a5113dc85a8bd809ae39a2156435ff18091c1
Alex Chartier <achartier@admiralsecure.com>
2024-07-05 06:51:46 -0400

    Updated for J4, fixed links for names with quotes in them

--- src/Extension/Switchuser.php
+++ src/Extension/Switchuser.php
@@ -1,4 +1,20 @@
 <?php
+
+namespace Skurvish\Plugin\System\Switchuser\Extension;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Session\Session;
+use Joomla\CMS\Table\Table;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\User\UserFactoryInterface;
+use Joomla\Database\DatabaseAwareInterface;
+use Joomla\Database\DatabaseAwareTrait;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
+
 /*------------------------------------------------------------------------
 # plg_system_switchuser - Switch User Plugin
 # ------------------------------------------------------------------------
@@ -12,26 +28,41 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 
-class plgSystemSwitchUser extends JPlugin
+class Switchuser extends CMSPlugin  implements SubscriberInterface, DatabaseAwareInterface
 {
-	function __construct(&$subject, $config = array())
+	use DatabaseAwareTrait;
+
+    protected $app; // Provided by the CSMPlugin interface
+
+	protected $db; // Provided by the CMSPlugin interface
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onAfterDispatch' => 'onAfterDispatch',
+            'onAfterInitialise' => 'onAfterInitialise',
+        ];
+    }
+
+	function __construct(&$subject, $config = [])
 	{
+		$this->autoloadLanguage = true;
 		parent::__construct($subject, $config);
-		JPlugin::loadLanguage('plg_system_switchuser', JPATH_ADMINISTRATOR);
+		$this->db = Factory::getContainer()->get("DatabaseDriver");
 	}
 	
-	function onAfterDispatch()
+	function onAfterDispatch(Event $event)
 	{
-		$app = JFactory::getApplication();
-		$option = JRequest::getCmd('option', '');
-		$task = JRequest::getCmd('view', $option == 'com_users' ? 'view' : '');
+		$input = $this->app->input;
+		$option = $input->get('option', '');
+		$task = $input->get('view', $option == 'com_users' ? 'view' : '');
 		
 		//if (!$app->isAdmin() || $option != 'com_users' || $task != 'view') {
-		if (!$app->isAdmin() || $option != 'com_users' || $task != 'users') {
+		if (!$this->app->isClient('administrator') || $option != 'com_users' || $task != 'users') {
 			return;
 		}
 
-		$document =& JFactory::getDocument();
+		$document = Factory::getDocument();
 		$content = $document->getBuffer('component');
 
 		$pattern = '/name="cid\[\]" value="(\d+)"/';
@@ -41,46 +72,48 @@ class plgSystemSwitchUser extends JPlugin
 			return;
 		}
 		$userIds = $matches[1];
-		$db =& JFactory::getDbo();
-		$query = 'SELECT id, username, name'
-			. ' FROM #__users'
-			. ' WHERE id IN ('.implode(',', $userIds).')'
-		;
-		$db->setQuery($query);
-		$users = $db->loadObjectList('id');
+		$query = $this->db->getQuery(true);
+		$query 
+			->select('id, username, name')
+			->from('#__users')
+			->where('id IN ('.implode(',', $userIds).')');
+		$this->db->setQuery($query);
+		$users = $this->db->loadObjectList('id');
 
-		
-		
 		$patterns = array();
 		$replacements = array();
 		foreach ($users as $userId => $user) {
 			
 			//$patterns[] = '|<a href="'.addcslashes(JRoute::_('index.php?option=com_users&amp;view=user&amp;task=edit&amp;cid[]='.(int)$userId), '?').'"[^>]+>\s*'.$user->name.'\s*</a>|';
 			//$replacements[] = '${0} <a href="'.JURI::root().'index.php?switchuser=1&uid='.$userId.'" target="_blank" title="'.JText::sprintf('SWITCHUSER_FRONT_END', $user->username).'"><img style="margin: 0 10px;" src="'.JURI::root().'plugins/system/switchuser/switchuser/images/frontend-login.png" alt="'.JText::_('SWITCHUSER_FRONT_END').'" /></a>';
-			$patterns[] = '|<a href="'.addcslashes(JRoute::_('index.php?option=com_users&amp;task=user.edit&amp;id='.(int)$userId), '?').'"[^>]+>\s*'.$user->name.'\s*</a>|';
-			$replacements[] = '${0} <a href="'.JURI::root().'index.php?option=com_users&switchuser=1&uid='.$userId.'" target="_blank" title="'.JText::sprintf('SWITCHUSER_FRONT_END', $user->username).'"><img style="margin: 0 10px;" src="'.JURI::root().'plugins/system/switchuser/switchuser/images/frontend-login.png" alt="'.JText::_('SWITCHUSER_FRONT_END').'" /></a>';
+			$patterns[] = '|<a href="'.addcslashes(Route::_('index.php?option=com_users&amp;task=user.edit&amp;id='.(int)$userId), '?').'"[^>]+>\s*'.htmlentities($user->name).'\s*</a>|';
+			$replacements[] = '${0} <a href="'.URI::root().'index.php?option=com_users&switchuser=1&uid='.$userId.'" target="_blank" title="'.sprintf(Text::_('SWITCHUSER_FRONT_END'), htmlentities($user->username)).'"><img style="margin: 0 10px;" src="'.URI::root().'media/switchuser/images/frontend-login.png" alt="'.sprintf(Text::_('SWITCHUSER_FRONT_END'), htmlentities($user->username)).'" /></a>';
+
 		}
+
 		$content = preg_replace($patterns, $replacements, $content);
 		$document->setBuffer($content, 'component');
 	}
 	function onAfterInitialise()
 	{
-		$app	=& JFactory::getApplication();
-		$db		=& JFactory::getDbo();
-		$user	=& JFactory::getUser();
-		$userId = JRequest::getInt('uid', 0);
+		$input = $this->app->input;
+		$query = $this->db->getQuery(true);
+		$user	= $this->app->getIdentity();
+		$userId = $input->get('uid', 0);
 		
-		if ($app->isAdmin() || !JRequest::getBool('switchuser', 0) || !$userId) {
+		if ($this->app->isClient('administrator')|| $input->get('switchuser', 0) || !$userId) {
 			return;
 		}
 		
 		if ($user->id == $userId) {
-			return $app->redirect('index.php', JText::_('SWITCHUSER_YOU_HAVE_ALREADY_LOGIN_AS_THIS_USER'));
+			$this->app->redirect('index.php', JText::_('SWITCHUSER_YOU_HAVE_ALREADY_LOGIN_AS_THIS_USER'));
+			return;
 		}
 		
 		if ($user->id) {
-			JError::raiseWarning('SOME_ERROR_CODE', JText::_('SWITCHUSER_YOU_HAVE_LOGIN_LOGOUT_FIRST'));
-			return $app->redirect('index.php');
+ 			$this->app->enqueueMessage(Text::_('SWITCHUSER_YOU_HAVE_LOGIN_LOGOUT_FIRST'), 'warning');
+			$this->app->redirect('index.php');
+			return;
 		}
 		
 		//$backendSessionId = JRequest::getVar(md5(JUtility::getHash('administrator')), null ,"COOKIE");
@@ -88,40 +121,40 @@ class plgSystemSwitchUser extends JPlugin
 		
 		$backendSessionId = JRequest::getVar(md5(JApplication::getHash('administrator')), null ,"COOKIE");
 
-
-		$query = 'SELECT userid'
-			. ' FROM #__session'
-			. ' WHERE session_id = '.$db->Quote($backendSessionId)
-			. ' AND client_id = 1'
-			. ' AND guest = 0'
-		;
-		$db->setQuery($query);
-		if (!$backendUserId = $db->loadResult()) {
-			JError::raiseWarning('SOME_ERROR_CODE', JText::_('SWITCHUSER_BACKEND_USER_SESSION_EXPIRED'));
-			return $app->redirect('index.php');
-		}
-		
-		$instance =& JFactory::getUser($userId);
-
-		// If _getUser returned an error, then pass it back.
-		if (JError::isError($instance)) {
-			$app->redirect('index.php');
+		$query 
+			->select('userid')
+			->form('#__session')
+			->where('session_id = ' . $db->Quote($backendSessionId))
+			->where('client_id = 1')
+			->where('guest = 0');
+		$this->db->setQuery($query);
+		if (!$backendUserId = $this->db->loadResult()) {
+ 			$this->app->enqueueMessage(Text::_('SWITCHUSER_BACKEND_USER_SESSION_EXPIRED'), 'warning');
+			$this->app->redirect('index.php');
 			return;
 		}
+		
+		$instance =  Factory::getContainer()->get(UserFactoryInterface::class)->loadUserById($userId); 
+
+		// If _getUser returned an error, then pass it back.
+//		if (JError::isError($instance)) {
+//			$app->redirect('index.php');
+//			return;
+//		}
 
 		// If the user is blocked, redirect with an error
 		if ($instance->get('block') == 1) {
-			JError::raiseWarning('SOME_ERROR_CODE', JText::_('E_NOLOGIN_BLOCKED'));
-			$app->redirect('index.php');
+ 			$this->app->enqueueMessage(Text::_('E_NOLOGIN_BLOCKED'), 'warning');
+			$this->app->redirect('index.php');
 			return;
 		}
 
 		// Get an ACL object
-		$acl =& JFactory::getACL();
+//		$acl = JFactory::getACL();
 
 		// Get the user group from the ACL
 		if ($instance->get('tmp_user') == 1) {
-			$grp = new JObject;
+			$grp = new stdClass();
 			// This should be configurable at some point
 			$grp->set('name', 'Registered');
 		} else {
@@ -151,11 +184,11 @@ class plgSystemSwitchUser extends JPlugin
 		$instance->set('usertype', $grp->name);
 
 		// Register the needed session variables
-		$session =& JFactory::getSession();
+		$session = Factory::getSession();
 		$session->set('user', $instance);
 
 		// Get the session object
-		$table = & JTable::getInstance('session');
+		$table = Table::getInstance('session');
 		$table->load( $session->getId() );
 
 		$table->guest 		= $instance->get('guest');
@@ -168,6 +201,6 @@ class plgSystemSwitchUser extends JPlugin
 
 		// Hit the user last visit field
 		$instance->setLastVisit();
-		$app->redirect('index.php', JText::_('SWITCHUSER_YOU_HAVE_LOGIN_SUCCESSFULLY'));
+		$this->app->redirect('index.php', Text::_('SWITCHUSER_YOU_HAVE_LOGIN_SUCCESSFULLY'));
 	}
 }
